e1fc7e201f717e4f8af7fbf47b5593e3b2bd75a2
Dinithiii04 <167051111+Dinithiii04@users.noreply.github.com>
2026-04-22 22:29:12 +0530

    Implement home page UI and content

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import FallsHistory from "@/src/modules/falls-history";
+
+export default function Home() {
+  return (
+    <main className="relative flex-1 overflow-hidden">
+      <FallsHistory />
+    </main>
+  );
+}
